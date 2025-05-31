959ffdd0cf011739f3156bca9509dbfd0ed30a09
superTO <to840116@gmail.com>
2025-05-31 19:13:29 +0800

    Feat: add kickstarter.py

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # playwright 
 
-## execution steps
+## 執行步驟
+1. conda env create -f environment.yml --prefix .venv
+- 直接使用 environment.yml 建立虛擬環境 (會一併將套件安裝好, 所以會執行比較久)
+2. conda conda activate .\.venv\
+3. playwright install
+4. python kickstarter.py https://www.kickstarter.com/projects/metismediarpg/astra-arcanum-the-roleplaying-game/community
+- 網址自行替換
+
+## playwright codegen execution steps
 pip install playwright
 playwright install
 
 python main.py
+playwright codegen https://www.kickstarter.com/
+```
+使用 playwright codegen 產生的程式碼會優先使用text拿資料, 不太適合用來爬蟲
+```
+
+## 使用 conda env export 匯出虛擬環境
+```
+conda env export > environment.yml
+```
---
+++ kickstarter.py
@@ -0,0 +1,109 @@
+import argparse
+import random
+import time
+from typing import List
+from playwright.sync_api import Playwright, sync_playwright
+# from playwright_stealth import stealth_sync, StealthConfig
+
+def run(playwright: Playwright, initial_url: str) -> List[str]:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    # context = browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36")
+    page = context.new_page()
+
+    ## 太多驗證啦
+    # 因為重開browser所以不用了
+    # stealth_config = StealthConfig(
+    #     webdriver=True,
+    #     webgl_vendor=True,
+    #     chrome_app=True,
+    #     chrome_csi=True,
+    #     chrome_load_times=True,
+    #     chrome_runtime=True,
+    #     iframe_content_window=True,
+    #     media_codecs=True,
+    #     navigator_hardware_concurrency=4,
+    #     navigator_languages=True,
+    #     navigator_permissions=True,
+    #     navigator_platform=True,
+    #     navigator_plugins=True,
+    #     navigator_user_agent=True,
+    #     navigator_vendor=True,
+    #     outerdimensions=True,
+    #     hairline=True,
+    #     vendor='Google Inc.',
+    #     renderer='Intel Iris OpenGL Engine',
+    #     nav_vendor='Google Inc.',
+    #     nav_user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
+    #     nav_platform="Win32",
+    #     languages=('en-US', 'en'),
+    #     runOnInsecureOrigins=None
+    # )
+
+    # stealth_sync(page, config=stealth_config)
+
+    page.goto(initial_url)
+
+    time.sleep(random.uniform(1, 5)) # 等1~5秒
+    # 先判斷是不是US再決定要不要爬
+    # page.get_by_role("link", name="United States").first.click()
+
+    ## a:has-text('United States') 若不需要請自行移除 
+    elements = page.locator(".secondary-text.js-location-secondary-text a:has-text('United States')").all()
+    href_list = []
+    for element in elements:
+        # print(element.all_inner_texts())
+        # 向上2層
+        parent = element.locator("xpath=../..")
+        # print(parent.all_inner_texts())
+        # 在同一層找 .primary-text.js-location-primary-text 並click第一個match項目
+        primary_text_element = parent.locator(".primary-text.js-location-primary-text").first
+        # print(primary_text_element.all_inner_texts())
+        ## 拿取 html a element
+        link_element = primary_text_element.locator("a").first
+        href = link_element.get_attribute("href")
+        href_list.append(href)
+    
+    # print(href_list)
+    # ---------------------
+    context.close()
+    browser.close()
+    return href_list
+
+# 驗證太煩了, 每次都重開一個新個browser
+def run2(playwright: Playwright, href_list: List) -> List[str]:
+    location = []
+    for href in href_list:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        full_url = "https://www.kickstarter.com" + href
+        
+        page.goto(full_url)
+        # 因為重開browser所以不用等了
+        # time.sleep(random.uniform(1, 5)) # 等1~5秒
+           
+        location_span = page.locator("#location_filter .js-title").first
+        location_text = location_span.inner_text()
+        location.append(location_text)
+        # print(f"当前选择的地点是: {location_text}")
+        context.close()
+        browser.close()
+    
+    return location
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Kickstarter Location Scraper")
+    parser.add_argument("url", help="The initial URL to start with")
+    args = parser.parse_args()
+
+    with sync_playwright() as playwright:
+        href_list = run(playwright, args.url)
+        print(href_list)
+        location_text_list = run2(playwright, href_list)
+        print(location_text_list)
+    
+
+
