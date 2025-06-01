f18ed7895e1c58b897d5e954929fb02c74dc92e1
superTO <to840116@gmail.com>
2025-06-01 23:32:28 +0800

    Feat: 輸出格式變更

--- kickstarter.py
+++ kickstarter.py
@@ -5,7 +5,7 @@ from typing import List
 from playwright.sync_api import Playwright, sync_playwright
 # from playwright_stealth import stealth_sync, StealthConfig
 
-def run(playwright: Playwright, initial_url: str) -> List[str]:
+def run(playwright: Playwright, initial_url: str) -> List[str|None]:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     # context = browser.new_context(user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36")
@@ -48,21 +48,24 @@ def run(playwright: Playwright, initial_url: str) -> List[str]:
     # 先判斷是不是US再決定要不要爬
     # page.get_by_role("link", name="United States").first.click()
 
-    ## a:has-text('United States') 若不需要請自行移除 
-    elements = page.locator(".secondary-text.js-location-secondary-text a:has-text('United States')").all()
+    # elements = page.locator(".secondary-text.js-location-secondary-text a:has-text('United States')").all()
+    elements = page.locator(".secondary-text.js-location-secondary-text").all()
     href_list = []
     for element in elements:
         # print(element.all_inner_texts())
-        # 向上2層
-        parent = element.locator("xpath=../..")
-        # print(parent.all_inner_texts())
-        # 在同一層找 .primary-text.js-location-primary-text 並click第一個match項目
-        primary_text_element = parent.locator(".primary-text.js-location-primary-text").first
-        # print(primary_text_element.all_inner_texts())
-        ## 拿取 html a element
-        link_element = primary_text_element.locator("a").first
-        href = link_element.get_attribute("href")
-        href_list.append(href)
+        if element.all_inner_texts()[0] != 'United States':
+            href_list.append(None)
+        else:
+            # 向上2層
+            parent = element.locator("xpath=../..")
+            # print(parent.all_inner_texts())
+            # 在同一層找 .primary-text.js-location-primary-text 並click第一個match項目
+            primary_text_element = parent.locator(".primary-text.js-location-primary-text").first
+            # print(primary_text_element.all_inner_texts())
+            ## 拿取 html a element
+            link_element = primary_text_element.locator("a").first
+            href = link_element.get_attribute("href")
+            href_list.append(href)
     
     # print(href_list)
     # ---------------------
@@ -71,25 +74,28 @@ def run(playwright: Playwright, initial_url: str) -> List[str]:
     return href_list
 
 # 驗證太煩了, 每次都重開一個新個browser
-def run2(playwright: Playwright, href_list: List) -> List[str]:
+def run2(playwright: Playwright, href_list: List) -> List[str|None]:
     location = []
     for href in href_list:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = context.new_page()
+        if href is None:
+            location.append(None)
+        else:
+            browser = playwright.chromium.launch(headless=False)
+            context = browser.new_context()
+            page = context.new_page()
 
-        full_url = "https://www.kickstarter.com" + href
-        
-        page.goto(full_url)
-        # 因為重開browser所以不用等了
-        # time.sleep(random.uniform(1, 5)) # 等1~5秒
-           
-        location_span = page.locator("#location_filter .js-title").first
-        location_text = location_span.inner_text()
-        location.append(location_text)
-        # print(f"当前选择的地点是: {location_text}")
-        context.close()
-        browser.close()
+            full_url = "https://www.kickstarter.com" + href
+
+            page.goto(full_url)
+            # 因為重開browser所以不用等了
+            # time.sleep(random.uniform(1, 5)) # 等1~5秒
+
+            location_span = page.locator("#location_filter .js-title").first
+            location_text = location_span.inner_text()
+            location.append(location_text)
+            # print(f"当前选择的地点是: {location_text}")
+            context.close()
+            browser.close()
     
     return location
 
